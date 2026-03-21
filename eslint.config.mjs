579654a6d01c50863ea5339dd06056a56ddfae30
Luke Horner <ljhorner2@gmail.com>
2026-03-21 11:54:37 +0000

    formatting and new eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,33 @@
-// @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
+import js from "@eslint/js";
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt([
+    js.configs.recommended,
+    {
+        files: ["**/*.vue", "**/*.js", "**/*.ts", "**/*.jsx", "**/*.cjs", "**/*.mjs"],
+        languageOptions: {
+            globals: {
+                defineNuxtConfig: true,
+                RequestInfo: true,
+            },
+            parserOptions: {
+                ecmaVersion: "latest",
+            },
+        },
+        rules: {
+            "vue/multi-word-component-names": "off",
+            "vue/html-self-closing": [
+                "error",
+                {
+                    html: {
+                        void: "always",
+                        normal: "always",
+                        component: "always",
+                    },
+                    svg: "always",
+                    math: "always",
+                },
+            ],
+        },
+    },
+]);
