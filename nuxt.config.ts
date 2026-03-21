36088462c20d8896dfdbf7eb8134b8fc13f97182
Luke Horner <ljhorner2@gmail.com>
2026-03-21 11:50:44 +0000

    disable ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-
-  modules: [
-    '@nuxt/eslint',
-    '@nuxtjs/google-fonts',
-    '@nuxtjs/tailwindcss'
-  ]
-})
+    compatibilityDate: "2025-07-15",
+    devtools: { enabled: true },
+    ssr: false,
+    modules: ["@nuxt/eslint", "@nuxtjs/google-fonts", "@nuxtjs/tailwindcss"],
+});
