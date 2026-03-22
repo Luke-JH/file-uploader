4d528665f4e450b78b13955aa71e59f6af32a242
Luke Horner <ljhorner2@gmail.com>
2026-03-22 18:59:28 +0000

    create base button component

---
+++ app/BaseButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <button
+        @click="emit('click', $event)"
+        class="bg-primary hover:bg-primary-hover text-white p-4 rounded-lg"
+    >
+        {{ text }}
+        <font-awesome v-if="icon" :icon="icon" size="lg" />
+    </button>
+</template>
+<script setup lang="ts">
+type Props = {
+    text?: string;
+    icon?: [string, string];
+};
+defineProps<Props>();
+
+type Emits = {
+    click: [event: MouseEvent];
+};
+const emit = defineEmits<Emits>();
+</script>
