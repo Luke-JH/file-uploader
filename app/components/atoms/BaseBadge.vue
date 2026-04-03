f2e5235c52e2c85e85a8b840d134b9712b59aae4
Luke Horner <ljhorner2@gmail.com>
2026-04-03 11:17:16 +0100

    badge group components

---
+++ app/components/atoms/BaseBadge.vue
@@ -0,0 +1,22 @@
+<template>
+    <span
+        :class="[
+            `${colorClasses.text} ${colorClasses.background}`,
+            'text-xs font-medium px-1.5 py-0.5 rounded',
+        ]"
+        >{{ text }}</span
+    >
+</template>
+<script setup lang="ts">
+type BaseBadgeProps = {
+    text: string;
+    // color: "green" | "blue";
+    colorClasses: {
+        text: string;
+        background: string;
+    };
+};
+defineProps<BaseBadgeProps>();
+
+export type { BaseBadgeProps };
+</script>
---
+++ app/components/molecules/BaseBadgeGroup.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="flex gap-3">
+        <BaseBadge
+            v-for="badge in badges"
+            :key="`${badge.text}Badge`"
+            :text="badge.text"
+            :color-classes="{
+                text: badge.colorClasses.text,
+                background: badge.colorClasses.background,
+            }"
+        />
+    </div>
+</template>
+<script setup lang="ts">
+import type { BaseBadgeProps } from "../atoms/BaseBadge.vue";
+
+type Props = {
+    badges: BaseBadgeProps[];
+};
+
+defineProps<Props>();
+</script>
