ac6d3f8c6b84c325f47fb1614b5f6312fb0e0ecf
Luke Horner <ljhorner2@gmail.com>
2026-04-03 10:49:17 +0100

    new card components

---
+++ app/components/atoms/BaseCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div
+        :class="[
+            transparent ? 'bg-transparent' : 'bg-zinc-300',
+            { 'border border-gray-100 rounded-2xl shadow-xl': !hideBorder },
+            ' w-full justify-center  p-8  ',
+        ]"
+    >
+        <slot />
+    </div>
+</template>
+<script setup lang="ts">
+type Props = {
+    transparent?: boolean;
+    hideBorder?: boolean;
+};
+defineProps<Props>();
+</script>
---
+++ app/components/molecules/PlaceholderCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <BaseCard :transparent="transparent" :hide-border="hideBorder">
+        <div class="flex flex-col items-center justify-center h-full gap-2">
+            <font-awesome v-if="icon" :icon="icon" size="2xl" />
+            <p v-if="text" class="text-center">{{ text }}</p>
+        </div>
+    </BaseCard>
+</template>
+<script setup lang="ts">
+type Props = {
+    text?: string;
+    icon?: [string, string];
+    transparent?: boolean;
+    hideBorder?: boolean;
+};
+defineProps<Props>();
+</script>
