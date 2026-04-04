b1d32d82f37fdfc93d32568637fa99c346f5c0b7
Luke Horner <ljhorner2@gmail.com>
2026-04-04 11:37:19 +0100

    new TechStackBadgeGroup component

---
+++ app/components/organisms/TechStackBadgeGroup.vue
@@ -0,0 +1,48 @@
+<template>
+    <BaseBadgeGroup
+        :badges="[
+            {
+                text: 'Nuxt 4',
+                colorClasses: {
+                    text: 'text-white',
+                    background: 'bg-emerald-500',
+                },
+            },
+            {
+                text: 'Vue 3',
+                colorClasses: {
+                    text: 'text-white',
+                    background: 'bg-emerald-600',
+                },
+            },
+            {
+                text: 'TypeScript',
+                colorClasses: {
+                    text: 'text-white',
+                    background: 'bg-blue-500',
+                },
+            },
+            {
+                text: 'TailwindCSS',
+                colorClasses: {
+                    text: 'text-white',
+                    background: 'bg-cyan-600',
+                },
+            },
+            {
+                text: 'ESLint',
+                colorClasses: {
+                    text: 'text-white',
+                    background: 'bg-purple-700',
+                },
+            },
+            {
+                text: 'Prettier',
+                colorClasses: {
+                    text: 'text-white',
+                    background: 'bg-pink-600',
+                },
+            },
+        ]"
+    />
+</template>
