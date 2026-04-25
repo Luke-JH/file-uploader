c8096fd9a8819666395e9c88daa5a6d5bb91895c
Luke Horner <ljhorner2@gmail.com>
2026-04-25 13:55:08 +0100

    button disable support

--- app/components/atoms/BaseButton.vue
+++ app/components/atoms/BaseButton.vue
@@ -1,8 +1,14 @@
 <template>
     <button
-        class="p-4 rounded-lg border border-primary text-primary hover:bg-primary hover:text-white"
-        :type="type ?? 'button'"
+        :class="[
+            disabled
+                ? 'opacity-50 cursor-not-allowed pointer-events-none'
+                : 'hover:bg-primary hover:text-white',
+            'p-4 rounded-lg border border-primary text-primary ',
+        ]"
         @click="emit('click', $event)"
+        :type
+        :disabled
     >
         {{ text }}
         <font-awesome v-if="icon" :icon="icon" size="lg" />
@@ -13,8 +19,9 @@ type Props = {
     text?: string;
     icon?: FontAwesomeIcon;
     type?: "button" | "reset" | "submit";
+    disabled?: boolean;
 };
-defineProps<Props>();
+const { text, icon, type = "button", disabled = false } = defineProps<Props>();
 
 type Emits = {
     click: [event: MouseEvent];
