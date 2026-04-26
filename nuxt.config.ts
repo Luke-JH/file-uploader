1dc0c15cd1646fc2e7ff2ea5e7d314fde09b60c2
Luke Horner <ljhorner2@gmail.com>
2026-04-26 13:50:53 +0100

    base URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
     compatibilityDate: "2025-07-15",
 
+    app: {
+        baseURL: "file-uploader",
+    },
+
     components: [
         {
             path: "~~/app",
